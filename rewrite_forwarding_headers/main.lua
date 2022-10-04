5b29b0b60f349a59309d607e2f9770a671047a59
Filip Bednárik <bednarik@essential-data.sk>
2022-10-04 17:58:58 +0200

    use ERR log level

--- rewrite_forwarding_headers/main.lua
+++ rewrite_forwarding_headers/main.lua
@@ -6,11 +6,11 @@ local ngx_log = ngx.log
 local _M = {}
 
 function _M.rewrite()
-  ngx_log(ngx.INFO, "Hello world")
+  ngx_log(ngx.ERR, "Hello world")
   if ngx.var.http_x_forwarded_proto then
-    ngx_log(ngx.INFO, string_format("X-FORWARDED-PROTO header: \"%s\"", ngx.var.http_x_forwarded_proto))
-    ngx_log(ngx.INFO, string_format("X-FORWARDED-FOR header: \"%s\"", ngx.var.http_x_forwarded_for))
-    ngx_log(ngx.INFO, string_format("X-FORWARDED-HOST header: \"%s\"", ngx.var.http_x_forwarded_host))
+    ngx_log(ngx.ERR, string_format("X-FORWARDED-PROTO header: \"%s\"", ngx.var.http_x_forwarded_proto))
+    ngx_log(ngx.ERR, string_format("X-FORWARDED-FOR header: \"%s\"", ngx.var.http_x_forwarded_for))
+    ngx_log(ngx.ERR, string_format("X-FORWARDED-HOST header: \"%s\"", ngx.var.http_x_forwarded_host))
   end
 end
 
