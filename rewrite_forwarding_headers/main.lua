5238f750f13c091f15b2359ac562b5d0a28fa117
Filip Bednárik <bednarik@essential-data.sk>
2022-10-04 18:40:40 +0200

    Fix

--- rewrite_forwarding_headers/main.lua
+++ rewrite_forwarding_headers/main.lua
@@ -6,11 +6,10 @@ local ngx_log = ngx.log
 local _M = {}
 
 function _M.rewrite()
-  ngx_log(ngx.ERR, string_format("X-FORWARDED-SCHEME header: \"%s\"", ngx.var.http_x_forwarded_scheme))
   ngx_log(ngx.ERR, string_format("X-FORWARDED-PROTO header: \"%s\"", ngx.var.http_x_forwarded_proto))
   ngx_log(ngx.ERR, string_format("X-FORWARDED-FOR header: \"%s\"", ngx.var.http_x_forwarded_for))
   ngx_log(ngx.ERR, string_format("X-FORWARDED-HOST header: \"%s\"", ngx.var.http_x_forwarded_host))
-  if ngx.var.http_x_forwarded_scheme == "https" then
+  if ngx.var.http_x_forwarded_proto == "https" then
     ngx_log(ngx.ERR, "Setting protocol to https")
     ngx.var.pass_access_scheme = "https"
   end
