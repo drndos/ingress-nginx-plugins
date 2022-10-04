094f05f6908c4dd3793440e6468257e82b6f7224
Filip Bednárik <bednarik@essential-data.sk>
2022-10-04 17:51:50 +0200

    Add test log

--- rewrite_forwarding_headers/main.lua
+++ rewrite_forwarding_headers/main.lua
@@ -6,6 +6,7 @@ local ngx_log = ngx.log
 local _M = {}
 
 function _M.rewrite()
+  ngx_log(ngx.INFO, "Hello world")
   if ngx.var.http_x_forwarded_proto then
     ngx_log(ngx.INFO, string_format("X-FORWARDED-PROTO header: \"%s\"", ngx.var.http_x_forwarded_proto))
     ngx_log(ngx.INFO, string_format("X-FORWARDED-FOR header: \"%s\"", ngx.var.http_x_forwarded_for))
